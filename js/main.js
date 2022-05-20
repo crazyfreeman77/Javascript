c94fa6c6e623a279c72951e8290e61114090d804
crazyfreeman77 <104940633+crazyfreeman77@users.noreply.github.com>
2022-05-20 13:32:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+'use strict';
+ // First exercise
+let letter = ['a', 'b', 'c'], 
+    nums = [1, 2, 3];
+let letterNumes = letter.concat(nums);
+
+console.log(letterNumes); 
+
+
+
+ // Second exercise
+let arrival = ['c', 'b', 'd'];
+    arrival.push(1, 2, 3);
+  
+    alert(arrival); 
+
+console.log(arrival);
+
+
+ // Third exercise
+let grade = [3, 4, 1, 2, 5];
+    document.write(grade.sort());
+
+console.log(grade);
+
+
+
+ // Fourth exercise
+let numArr = [1, 2, 3,]
+let numArrReversed = numArr.reverse();
+
+console.log(numArrReversed);
